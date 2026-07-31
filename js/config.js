8a3140cd503ac39fcbe034cac88aba67208d3353
Rattililquran <oemarz69@gmail.com>
2026-07-31 15:23:38 +0700

    fix: update BACKEND_URL ke deployment terbaru yang sudah diauthorize

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 var CONFIG = {
   // URL Web App Google Apps Script
   // Ganti dengan URL deployment Anda
-  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxZf_bBz7oZ4OhXsJHwTJxXLvv06ks3-eFCU6xANC8VHnRkLFcLIUxj2glLaod2BzK3/exec',
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbwsylfQbAUpkOb48xnSZuPaP29aY8n2reNhq_VxhwT77wxiRGmmROb17uLeBjATBfF7/exec',
 
   // Nama institusi (tampil di UI)
   NAMA_INSTITUSI: 'Rattilil Qur\'an',
